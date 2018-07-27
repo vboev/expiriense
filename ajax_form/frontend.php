0491ac73aafd67752b62674056935a17a2d524bf
Vlad <ash81@mail.ru>
2018-07-27 10:50:41 +0300

    дабавлено код и правка для очистки формы

---
+++ ajax_form/frontend.php
@@ -0,0 +1,15 @@
+<form id="callback-form">
+<div class="messages"></div>
+    <br/>
+    <label>Ваше имя:</label><br/>
+    <input type="text" id="user_name" value="" /><br/>
+     
+    <label>Ваш e-mail:</label><br/>
+    <input type="text" id="user_email" value="" /><br/>
+     
+    <label>Текст сообщения:</label><br/>
+    <textarea id="text_comment"></textarea>
+     
+    <br/>
+    <input type="button" value="Отправить" id="btn_submit" />   
+    </form>
---
+++ ajax_form/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+            $('#btn_submit').click(function(){
+                // собираем данные с формы
+                var user_name    = $('#user_name').val();
+                var user_email   = $('#user_email').val();
+                var text_comment = $('#text_comment').val();
+                // отправляем данные
+                $.ajax({
+                    url: "action.php", // куда отправляем
+                    type: "post", // метод передачи
+                    dataType: "json", // тип передачи данных
+                    data: { // что отправляем
+                        "user_name":    user_name,
+                        "user_email":   user_email,
+                        "text_comment": text_comment
+                    },
+                    // после получения ответа сервера
+                    success: function(data){
+                        $('.messages').html(data.result); // выводим ответ сервера
+                        if (data.success) {
+                            $('#callback-form')[0].reset();
+                        }
+                    }
+                });
+            });
+        });
